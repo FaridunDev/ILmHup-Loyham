9fe647cfae40114480909cfdee20ab6e05f26f61
Faridun <faridun@example.com>
2026-03-07 13:11:42 +0500

    fix: login viewni MyTokenObtainPairView ga almashtirish

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, ProfileView
+from .views import RegisterView, ProfileView, MyTokenObtainPairView, MyTokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('login/', MyTokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', MyTokenRefreshView.as_view(), name='token-refresh'),
     path('profile/', ProfileView.as_view(), name='profile'),
 ]
