a47122dee111fd662db1b7a4d0e905b55f0174a9
Faridun <faridun@example.com>
2026-03-08 12:30:43 +0500

    fix: Register response ga full_name qo'shildi

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
 User = get_user_model()
-
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
@@ -13,15 +11,15 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         data['role'] = 'instructor' if self.user.is_instructor else 'student'
         return data
 
-
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
     password2 = serializers.CharField(write_only=True)
-    full_name = serializers.CharField(write_only=True, required=False, allow_blank=True)
+    full_name = serializers.CharField(write_only=False, required=False, allow_blank=True, default='')
 
     class Meta:
         model = User
         fields = ('id', 'email', 'username', 'full_name', 'password', 'password2', 'is_instructor')
+        read_only_fields = ('id',)
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -33,16 +31,20 @@ class RegisterSerializer(serializers.ModelSerializer):
         password = validated_data.pop('password')
         full_name = validated_data.pop('full_name', '')
         user = User(**validated_data)
+        user.set_password(password)
         if full_name:
             names = full_name.split(' ', 1)
             user.first_name = names[0]
             user.last_name = names[1] if len(names) > 1 else ''
-        user.set_password(password)
         if validated_data.get('is_instructor'):
             user.is_student = False
         user.save()
         return user
 
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['full_name'] = instance.get_full_name() or instance.username
+        return data
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
